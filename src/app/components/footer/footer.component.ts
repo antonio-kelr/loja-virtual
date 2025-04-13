7467ca3cd74f54642ca18d6318511ec4d3523b95
Antonio Kelr <antoniofilhodaconceicaorozafil@gmail.com>
2025-04-12 22:20:44 -0300

    adiciona componente Footer ao app e ajusta a estrutura do template

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faFacebook, faInstagram, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, faPhone, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterModule,
+    FontAwesomeModule,
+    FormsModule,
+    ButtonModule,
+    InputTextModule
+  ],
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent {
+  faFacebook = faFacebook;
+  faInstagram = faInstagram;
+  faTwitter = faTwitter;
+  faYoutube = faYoutube;
+  faEnvelope = faEnvelope;
+  faPhone = faPhone;
+  faMapMarkerAlt = faMapMarkerAlt;
+
+  email: string = '';
+
+  onSubmit() {
+    if (this.email) {
+      // Aqui você pode implementar a lógica para enviar o email
+      console.log('Email cadastrado:', this.email);
+      this.email = '';
+    }
+  }
+}
